18ae6c7bb66c7a5f880ef37110b900be6ee9a270
MAKSAT <maksatkanybekov2004@gmail.com>
2022-12-26 20:09:18 +0600

    departure and social

--- app.js
+++ app.js
@@ -22,3 +22,28 @@ function sliderActive(i) {
     sliders[zero].style.display = 'block';
     dots[zero].className += " active"
 }
+
+
+const message = document.getElementById("message");
+const whatsapp = document.getElementById("whatsapp");
+const instagram = document.getElementById("instagram");
+const telegram = document.getElementById("telegram")
+const messageAll = document.getElementById("message-all");
+const messageClose = document.getElementById("message-close");
+
+
+    message.addEventListener("click", () => {
+      whatsapp.classList.toggle("whatsapp-active");
+      instagram.classList.toggle("instagram-active");
+      telegram.classList.toggle("telegram-active")
+      messageClose.classList.toggle("message-close__active");
+      messageAll.classList.toggle("message-all__active");
+    });
+
+
+const phone = document.querySelector('.tel')
+const tele = document.querySelector('.phone-tele')
+
+phone.addEventListener('click', () => {
+    tele.classList.toggle("phone-tele__active")
+})
